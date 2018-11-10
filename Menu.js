df0f7bfe516a94bda47cee4c9ff061467c8ea2c3
LifeH <hwie.zhu@gmail.com>
2018-11-10 10:31:29 -0500

    event handler

--- Menu.js
+++ Menu.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
 export class Menu extends React.Component {
+
+handleClick: function(e){
+  const text=e.target.value;
+  this.props.chooseVideo(text);
+}
+//step 7: handleClick event that passes chooseVideo function with text variable
   render() {
     return (
       //step 6: chooseVideo passing down as props on Menu component
