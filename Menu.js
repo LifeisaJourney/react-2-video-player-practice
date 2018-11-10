5a5066cb3e7d52ed886ecefb36804e704d2f966b
LifeH <hwie.zhu@gmail.com>
2018-11-10 10:24:06 -0500

    adding onClick with chooseVideo as props

--- Menu.js
+++ Menu.js
@@ -3,7 +3,8 @@ import React from 'react';
 export class Menu extends React.Component {
   render() {
     return (
-      <form>
+      //step 6: chooseVideo passing down as props on Menu component
+      <form onClick={this.props.chooseVideo}>
         <input type="radio" name="src" value="fast" /> fast
         <input type="radio" name="src" value="slow" /> slow
         <input type="radio" name="src" value="cute" /> cute
